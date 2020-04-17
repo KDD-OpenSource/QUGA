648dcb23d711e7f73da0be7622a992b7762bf4d4
Benedikt Boeing <boeing@bit.uni-bonn.de>
2020-04-17 11:49:16 +0200

    Update main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Main Module of AE_SMT Framework
 """
-
+import matplotlib
+matplotlib.use('Agg')
 from src.utils.executeExperiments import executeExperiments
 from src.utils.experimentSettings import ExperimentSettings
 from src.utils.objectCreator import objectCreator, getAlgorithms, getDatasets, getSmts, getResults
@@ -14,13 +15,51 @@ def main():
 
 def getExperimentSettings():
 	settings = ExperimentSettings()
-	settings.testName = 'EveryResultSineWavePart'
-	settings.description = ''
-	settings.experimentScope = 'ae_smt'
-	# settings.experimentScope = 'smt'
-	settings.resultFolders = None
-	# settings.resultFolders = ['C:\\Users\\Ben\\Desktop\\Projekte\\Eigene Paper\\AE+SMT\\Experiments\\AE_SMT\\results\\2020.02.27\\51431\\autoe_f08e6\\sineN_74d06']
+	# settings.testName = 'OrigReconQualSmallKitchen'
+	# settings.testName = 'MoxingBoxesWithLargerCirclesIn10Steps_takingHalfSinewaves'
+	# settings.testName = 'MoxingBoxesThroughMiddleWith50Dims'
+	# settings.testName = 'CreatingAGoodOrigReconSineCurvePlot'
+	# settings.testName = 'NewDataSet_KitchenAppliances_MonEvening'
+	# settings.testName = 'GrowingBoxAroundSineTrainPointCorrected'	
+	# settings.testName = 'AdversAttackOnSmallCluster_DifferentImbalancedDatasets_WithMaxErrorEsts'	
+	# settings.testName = 'TestECGReconstruction_WindowStep35_200Epochs_AVGError'
+	# settings.testName = 'ECG_WithAdversAttack_WithSamplEsts'
+	# settings.testName = 'ECG_WithSamplEsts_2_25'
+	# settings.testName = 'Synthetic_MaxAdversWithSamplEstsUptoK_25'
 
+
+	# settings.testName = 'FindOutBox_ECG'
+	# settings.testName = 'Frequency_TestSet'
+
+
+	# settings.testName = 'ParallelPlotECGTestSet'
+
+
+	# settings.testName = 'ParallelPlotECGTrainSet_140TimeSteps'
+	# settings.testName = 'SyntheticDataFullExperiment' 
+	# settings.testName = 'RealDataFullExperiment_200Epochs_FOR_AVERAGE_ERROR' 
+	# settings.testName = 'RealDataFullExperiment_WithOutOrigReconPlot' 
+	# settings.testName = 'RealDataFullExperiment_WithOutOrigReconPlot_WithoutSampling' 
+	# settings.testName = 'SyntheticDataFull_WithoutOrigRecon_WithoutSampling' 
+	# settings.testName = 'SyntheticDataFull_WithoutOrigRecon_WithSampling_HopeBetterReconstruction' 
+	# settings.testName = 'TestSynthMaxAdversAttackScale' 
+	# settings.testName = 'TestSynthMaxAdversAttackScale' 
+	# settings.testName = 'FULLSYNTHETIC_20_60_100_Epochs' 
+	# settings.testName = 'RealECG_200_Epochs_k_25' 
+	# settings.testName = 'TestAEParamPlots' 
+	# settings.testName = 'TestTimeMaxErrorPlot'
+	# settings.testName = 'TestLInftyEst'
+	# settings.testName = 'PlottingTimeVSErrors_2Hours'
+	settings.testName = 'NONTEST_PlottingTimeVSErrors_2Hours_OLDAESYN'
+
+
+	# settings.testName = 'NumSamplesForEstimationVSmaxAdversEst'	
+
+	# settings.testName = 'TwoSineCurves_MultipleFrequencies_NonAveraging'	
+	settings.description = ''
+	# settings.experimentScope = 'ae_smt'
+	settings.experimentScope = 'smt'
+	settings.resultFolders = None
 	return settings
 
 
@@ -33,4 +72,3 @@ def getExperimentObjects(seed):
 
 
 main()
-
